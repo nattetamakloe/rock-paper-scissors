46f0a551d71a0d20336c61e4859e888ad55b12f0
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-08-17 14:39:41 +0200

    convert random number to string of rock, paper, Scissors

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const choice = getComputerChoice();
 console.log(choice)
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+    let choice = Math.floor(Math.random() * 3) + 1;
+
+    if (choice === 1) {
+        return "Rock"
+    } else if (choice === 2) {
+        return "Paper"
+
+    } else if (choice === 3) {
+        return "Scissors"
+
+    }
+
 }
